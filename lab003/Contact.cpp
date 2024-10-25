2dd9988e78d052a92315302b29165cf75a5ccdc5
eofid <arhipadynec@gmail.com>
2024-10-25 14:52:01 +0300

    Contact.cpp

--- lab003/Contact.cpp
+++ lab003/Contact.cpp
@@ -7,27 +7,4 @@ Contact::Contact(const std::string& name, const std::string& phone, const std::s
 
 
 
-// Дружественная функция для перегрузки оператора ввода
-std::istream& operator>>(std::istream& is, Contact& contact) {
-    std::getline(is, contact.name);
-    std::getline(is, contact.phone);
-    std::getline(is, contact.email);
 
-    std::string street;
-    std::string city;
-    std::string postalCode;
-    std::getline(is, street);
-    std::getline(is, city);
-    std::getline(is, postalCode);
-
-    contact.address = Address(street, city, postalCode);
-
-    std::string companyName;
-    std::string position;
-    std::getline(is, companyName);
-    std::getline(is, position);
-
-    contact.company = Company(companyName, position);
-
-    return is;
-}
