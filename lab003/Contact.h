a585c74268899281ef3370450d2356c931d07061
eofid <arhipadynec@gmail.com>
2024-10-25 14:24:04 +0300

    Contact.h

--- lab003/Contact.h
+++ lab003/Contact.h
@@ -14,20 +14,9 @@ public:
     Address address;
     Company company;
 
-    // Прототип конструктора
     Contact(const std::string& name, const std::string& phone, const std::string& email, const Address& address, const Company& company);
 
-    // Скрытый друг - перегрузка оператора ==
-    friend bool operator==(const Contact& lhs, const Contact& rhs) {
-        return (lhs.name == rhs.name) && (lhs.phone == rhs.phone) && (lhs.email == rhs.email) &&
-               (lhs.address.getStreet() == rhs.address.getStreet()) &&
-               (lhs.address.getCity() == rhs.address.getCity()) &&
-               (lhs.address.getPostalCode() == rhs.address.getPostalCode()) &&
-               (lhs.company.getName() == rhs.company.getName()) &&
-               (lhs.company.getPosition() == rhs.company.getPosition());
-    }
-
-    // Дружественные операторы для потоков
+    // Объявляем перегруженные операторы как скрытых друзей
     friend std::ostream& operator<<(std::ostream& os, const Contact& contact);
     friend std::istream& operator>>(std::istream& is, Contact& contact);
 };
