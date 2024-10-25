fa1e90fe9c98753cb3d0e50b47e290b919698630
eofid <arhipadynec@gmail.com>
2024-10-25 14:24:30 +0300

    Contact.cpp

--- lab003/Contact.cpp
+++ lab003/Contact.cpp
@@ -10,7 +10,7 @@ std::ostream& operator<<(std::ostream& os, const Contact& contact) {
     os << contact.name << "\n" << contact.phone << "\n" << contact.email << "\n";
     os << contact.address.getStreet() << "\n" << contact.address.getCity() << "\n" << contact.address.getPostalCode() << "\n";
     os << contact.company.getName() << "\n" << contact.company.getPosition() << "\n";
-    return os;  // Возвращаем поток
+    return os;
 }
 
 // Реализация оператора ввода (для чтения из файла или потока)
@@ -19,21 +19,18 @@ std::istream& operator>>(std::istream& is, Contact& contact) {
     std::getline(is, contact.phone);
     std::getline(is, contact.email);
 
-    std::string street;
-    std::string city;
-    std:: string postalCode;
+    std::string street, city, postalCode;
     std::getline(is, street);
     std::getline(is, city);
     std::getline(is, postalCode);
 
-    contact.address = Address(street, city, postalCode); // Присваиваем адрес
+    contact.address = Address(street, city, postalCode);
 
-    std::string companyName;
-    std:: string position;
+    std::string companyName, position;
     std::getline(is, companyName);
     std::getline(is, position);
 
-    contact.company = Company(companyName, position); // Присваиваем компанию
+    contact.company = Company(companyName, position);
 
-    return is;  // Возвращаем поток
+    return is;
 }
